c1a0c45a78013d1741c4cc8d1e2e7435d6e400d5
bobkoobg <boyko.surlev@gmail.com>
2015-03-02 07:30:45 +0100

    Testing code structure + Interface added.

---
+++ src/main/java/interfaces/ControllerInterface.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interfaces;
+
+import control.Controller.ProposedElectiveSubject;
+import control.Controller.User;
+import control.Controller.Vote;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author root
+ */
+public interface ControllerInterface {
+
+    //Users
+    String authenticateUser(String userName, String password);
+    
+    String registerUser(User u);
+
+    String deleteParticularUser(String userName);
+    //missing updateUser
+    
+    //Subjects
+    ArrayList<ProposedElectiveSubject> getAllAvailableProposedElectiveSubjects();
+    
+    ProposedElectiveSubject addProposedElectiveSubject(ProposedElectiveSubject pes);
+    //missing update, delete subject
+    
+    //Votes
+    String addVoteFromParticularUser(ArrayList<Vote> votes);
+
+    ArrayList<Vote> getAllVotesOfParticularUser();
+
+    String deleteAllVotesOfParticularUser();
+
+    Vote updateParticularVoteOfParticularUser(Vote nv);
+}
